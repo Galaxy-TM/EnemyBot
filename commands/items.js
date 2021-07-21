a543ac460caa3ea5b049792b578ca3b7e81ea352
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-07-21 15:13:54 +0800

    whitespace.

--- commands/items.js
+++ commands/items.js
@@ -7,15 +7,15 @@ module.exports = (message) => {
     message.channel.send(new Discord.MessageEmbed()
         .setTitle("Items:")
         .addFields({
-            name: "Internal                    \u200c",
+            name: "Internal                         \u200c",
             value: Object.keys(NAMES).join("\n"),
             inline: true
         },{
-            name: "Display                    \u200c",
+            name: "Display                         \u200c",
             value: Object.entries(NAMES).map(([_i, name]) => name[0]).join("\n"),
             inline: true
         },{
-            name: "Emoji                    \u200c",
+            name: "Emoji                         \u200c",
             value: Object.keys(NAMES).map(item => EMOJIS[item] || "`[MISSING]`").join("\n"),
             inline: true
         })
