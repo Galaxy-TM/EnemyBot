7a71a02bbd0c5c10ad244b6e077a25cc476fd7b6
NKY5223 <76890154+NKY5223@users.noreply.github.com>
2021-07-24 12:17:46 +0800

    fixxx

--- index.js
+++ index.js
@@ -57,7 +57,51 @@ function toTime(ms = 0) {
     return `${hours} hour${hours === 1 ? "" : "s"} and ${minutes} minute${minutes === 1 ? "" : "s"}`;
 }
 
+const categories = ["cell", "craft", "other"];
+const CATEGORYNAMES = {
+    cell: `${EMOJIS.mover} Cells`,
+    craft: `${EMOJIS.arrow_shooter} Crafts`,
+    other: `⚙ Others`,
+    admin: `🔧 Admin`
+};
 
+const embed = new Discord.MessageEmbed()
+    .setTitle("Command List:")
+    .setColor("#E82727")
+    .addFields(categories.map(category => ({
+        name: `${CATEGORYNAMES[category]}: `,
+        value: Object.entries(commands).filter(([_n, command]) => command.category === category).map(([name]) => `\`${prefix}${name}\``).join(", ") || "[EMPTY]"
+    })));
+const adminEmbed = new Discord.MessageEmbed()
+    .setTitle("Command List:")
+    .setColor("#E82727")
+    .addFields([...categories, "admin"].map(category => ({
+        name: `${CATEGORYNAMES[category]}: `,
+        value: Object.entries(commands).filter(([_n, command]) => command.category === category).map(([name]) => `\`${prefix}${name}\``).join(", ") || "[EMPTY]"
+    })));
+
+const embeds = Object.fromEntries([].concat(...Object.entries(commands).filter(([_n, command]) => command.perms === "NORMAL").map(([cmdName, command]) => command.aliases.map(
+    alias => [alias, new Discord.MessageEmbed()
+        .setTitle(`Help: ${cmdName} ${EMOJIS[cmdName]}`)
+        .setDescription(command.description)
+        .addField(
+            `Aliases: ${command.aliases.join(", ")}`,
+            `Syntax: \`\`\`\n${command.syntax}\n\`\`\``
+        )
+        .setColor("#E82727")
+    ]
+))));
+const adminEmbeds = Object.fromEntries([].concat(...Object.entries(commands).map(([cmdName, command]) => command.aliases.map(
+    alias => [alias, new Discord.MessageEmbed()
+        .setTitle(`Help: ${cmdName} ${EMOJIS[cmdName]}`)
+        .setDescription(command.description)
+        .addField(
+            `Aliases: ${command.aliases.join(", ")}`,
+            `Syntax: \`\`\`\n${command.syntax}\n\`\`\``
+        )
+        .setColor("#E82727")
+    ]
+))));   
 /** @type { { [cmdName: string]: Command } } */
 const commands = {
     search: {
@@ -125,7 +169,16 @@ const commands = {
         syntax: `${prefix}help - Get a list of command\n${prefix}help <command> - Get info about the command`,
         description: `See a list of commands`,
 
-        func: require("./commands/help")
+        func: (message, _c, [cmdArg]) => {
+            if (cmdArg) {
+                message.channel.send(new Discord.MessageEmbed((ADMINIDS.includes(message.author.id) ? adminEmbeds : embeds)[cmdArg] || new Discord.MessageEmbed()
+                    .setTitle(`Could not find command \`${cmdArg}\``)
+                    .setColor("#E82727")
+                ));
+            } else {
+                message.channel.send(new Discord.MessageEmbed((ADMINIDS.includes(message.author.id) ? adminEmbed : embed)));
+            }
+        }
     },
     give: {
         cooldown: 1000,
@@ -197,7 +250,6 @@ const commands = {
         perms: "ADMIN"
     }
 };
-module.exports = { commands, ADMINIDS, prefix };
 
 // discord bot stuff
 client.once("ready", () => {
